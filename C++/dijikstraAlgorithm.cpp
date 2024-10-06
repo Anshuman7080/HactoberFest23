3db195a99ee194784b2971b40b8fb9fbad0d4163
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2024-10-06 14:31:55 +0000

    more optimized and cleaner version

--- C++/dijikstraAlgorithm.cpp
+++ C++/dijikstraAlgorithm.cpp
@@ -1,71 +1,70 @@
-/*
-Algorithm for finding shortest path(single source,single destination) in a graph. 
-  */
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define ipair pair<long long,long long>
-#define MAXN 110000
-#define inf 1LL<<55
-vector<ipair>adj_list[MAXN];
-vector<long long>dis(MAXN,inf);
-vector<long long>parent(MAXN,0);
 
-void dijikstra(ll src)
-{
-          priority_queue<ipair,vector<ipair>,greater<ipair>>pq;
-          dis[src]=0;
-          pq.push(make_pair(dis[src],src));
-          
-          while(!pq.empty())
-          {
-                    ll u=pq.top().second;
-                    pq.pop();
-                    for(auto p:adj_list[u])
-                    {
-                              ll v=p.first;
-                              ll weight=p.second;
-                              if(dis[v]>dis[u]+weight)
-                              {
-                              dis[v]=dis[u]+weight;
-                              parent[v]=u;
-                              pq.push(make_pair(dis[v],v));
-                              }
-                    }
-          }
+#define ll long long
+#define ipair pair<ll, ll>
+#define MAXN 110000
+#define INF LLONG_MAX
+
+vector<ipair> adj_list[MAXN];
+vector<ll> dis(MAXN, INF);
+vector<ll> parent(MAXN, -1);
+
+void dijkstra(ll src) {
+    priority_queue<ipair, vector<ipair>, greater<ipair>> pq;
+    dis[src] = 0;
+    pq.emplace(dis[src], src);
+
+    while (!pq.empty()) {
+        ll u = pq.top().second;
+        pq.pop();
+
+        // Process each neighbor of u
+        for (const auto& p : adj_list[u]) {
+            ll v = p.first;
+            ll weight = p.second;
+
+            // Relaxation step
+            if (dis[v] > dis[u] + weight) {
+                dis[v] = dis[u] + weight;
+                parent[v] = u;
+                pq.emplace(dis[v], v);
+            }
+        }
+    }
 }
-int main()
-{
-          ll v;ll e;cin>>v>>e;
-          for(ll i=0;i<e;i++)
-          {
-                    ll u;ll v;ll cost;cin>>u>>v>>cost;
-                    adj_list[u].push_back(make_pair(v,cost));
-                    adj_list[v].push_back(make_pair(u,cost));
-          }
-          parent[1]=-1;
-          dijikstra(1);
-          
-          if(parent[v]==0)
-          {
-                    cout<<-1<<endl;
-                    return 0;
-          }
-          stack<ll>path;
-          path.push(v);
-          ll i=v;
-          while(parent[i]!=-1)
-          {
-              path.push(parent[i]);
-              i=parent[i];
-          }
-          
-          while(!path.empty())
-          {
-                    cout<<path.top()<<" ";
-                    path.pop();
-          }
-          return 0;
+
+void printPath(ll destination) {
+    if (dis[destination] == INF) {
+        cout << -1 << endl; // No path found
+        return;
+    }
+
+    stack<ll> path;
+    for (ll i = destination; i != -1; i = parent[i]) {
+        path.push(i);
+    }
+
+    while (!path.empty()) {
+        cout << path.top() << " ";
+        path.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    ll v, e;
+    cin >> v >> e;
+
+    for (ll i = 0; i < e; i++) {
+        ll u, v, cost;
+        cin >> u >> v >> cost;
+        adj_list[u].emplace_back(v, cost);
+        adj_list[v].emplace_back(u, cost); // Assuming undirected graph
+    }
+
+    dijkstra(1); // Start Dijkstra from vertex 1
+    printPath(v); // Print the shortest path to vertex v
+
+    return 0;
 }
